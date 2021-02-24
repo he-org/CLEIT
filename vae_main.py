9eadb2b8ec12050efcf6bdd35fe36fdf7efa203d
Hidden He <he15810251026@gmail.com>
2021-02-23 21:43:31 -0800

    Update vae_main.py

--- vae_main.py
+++ vae_main.py
@@ -87,11 +87,6 @@ def main(args, update_params_dict):
     data_provider = DataProvider(batch_size=training_params['unlabeled']['batch_size'],
                                  target=args.measurement)
 
-    if "vae.pt" in os.listdir(training_params['model_save_folder']):
-        training_params.update(
-            {'retrain_flag': False}
-        )
-
     training_params.update(
         {
             'input_dim': data_provider.shape_dict[args.omics],
