5ec0327d36f4a493b188e496b9eb9341612e7a93
Hidden He <he15810251026@gmail.com>
2021-05-31 20:25:31 -0700

    Update train_cleitc.py

--- train_cleitc.py
+++ train_cleitc.py
@@ -30,8 +30,7 @@ def cleit_train_step(ae, reference_encoder, transmitter, batch, device, optimize
     loss = loss_dict['loss'] + code_loss
     optimizer.zero_grad()
     print(code_loss.item())
-    print(x_g_code)
-    print(list(ae.encoder.modules())[0][0].weight.grad)
+    print(list(ae.encoder.modules())[0])
 
     loss.backward()
     # cleit_params = [
