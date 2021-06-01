d8d999040a589bab052efa462e391dd18cca41dd
Hidden He <he15810251026@gmail.com>
2021-06-01 08:37:24 -0700

    Update train_cleitc.py

--- train_cleitc.py
+++ train_cleitc.py
@@ -33,6 +33,8 @@ def cleit_train_step(ae, reference_encoder, transmitter, batch, device, optimize
 
     loss.backward()
     if torch.isnan(x_m_code).any():
+        print(loss_dict)
+        print(code_loss)
         print(torch.sum(torch.isnan(x_m_code),dim=1))
         print(torch.isnan(list(ae.encoder.modules())[0][-1].weight).any())
         print(list(ae.encoder.modules())[0][-1].weight.grad)
